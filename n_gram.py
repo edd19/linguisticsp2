6de8a9fce9c1426ab1d33c72a22d890025186324
edd19 <eddyndizera@gmail.com>
2016-10-28 11:39:46 +0200

    bigram implemented

--- n_gram.py
+++ n_gram.py
@@ -7,14 +7,14 @@ class NGram(object):
         self.n = n
 
     def add_corpus(self, corpus):
-        for word in corpus.split():
-            self.update_counts(word)
+        for n_gram in self.get_n_grams(corpus):
+            self.update_counts(n_gram)
 
-    def update_counts(self, word):
-        if word in self.counts:
-            self.counts[word] += 1
+    def update_counts(self, n_gram):
+        if n_gram in self.counts:
+            self.counts[n_gram] += 1
         else:
-            self.counts[word] = 1
+            self.counts[n_gram] = 1
 
     def get_counts(self):
         return self.counts
@@ -22,10 +22,41 @@ class NGram(object):
     def flush(self):
         self.counts = {}
 
+    def get_next_word(self, corpus):
+        for sentence in self.split_corpus_sentences(corpus):
+            pass
+
+    @staticmethod
+    def split_corpus_sentences(corpus):
+        corpus_left = corpus
+        separator = "</s>"  # Signify end of sentence
+        while len(corpus_left) > 0:
+            sentence, sep, corpus_left = corpus_left.partition(separator)
+            sentence += separator
+            yield sentence.strip()
+
+    @staticmethod
+    def split_sentence_n_grams(sentence, n):
+        words = sentence.split()
+        n_gram = []
+        for word in words:
+            n_gram.append(word)
+            if len(n_gram) >= n:
+                yield " ".join(n_gram)
+                n_gram.pop(0)
+
+    def get_n_grams(self, corpus):
+        for sentence in self.split_corpus_sentences(corpus):
+            for n_gram in self.split_sentence_n_grams(sentence, self.n):
+                yield n_gram
+
 
 class Unigram(NGram):
     def __init__(self):
         super(self.__class__, self).__init__(n=1)
 
 
+class Bigram(NGram):
+    def __init__(self):
+        super(self.__class__, self).__init__(n=2)
 
--- test/test_n_gram.py
+++ test/test_n_gram.py
@@ -6,30 +6,56 @@ import n_gram
 
 class TestNGram(unittest.TestCase):
     def setUp(self):
-        self.s1 = "<s> HELLO MY DEAR FRIEND </s>"
-        self.s2 = "<s> HELLO MY DEAR FRIEND </s>\n<s> HOW ARE YOU MY FRIEND </s>"
+        self.corpus1 = "<s> HELLO MY DEAR FRIEND </s>"
+        self.corpus2 = "<s> HELLO MY DEAR FRIEND </s>\n<s> HOW ARE YOU MY FRIEND </s>"
 
     def test_unigram(self):
         unigram = n_gram.Unigram()
-        unigram.add_corpus(self.s1)
+        unigram.add_corpus(self.corpus1)
         expected = {"<s>": 1, "HELLO": 1, "MY": 1, "DEAR": 1, "FRIEND": 1, "</s>": 1}
         actual = unigram.get_counts()
         self.assertEqual(expected, actual)
 
         unigram.flush()
-        unigram.add_corpus(self.s2)
+        unigram.add_corpus(self.corpus2)
         expected = {"<s>": 2, "HELLO": 1, "MY": 2, "DEAR": 1, "FRIEND": 2, "</s>": 2, "HOW": 1, "ARE": 1, "YOU": 1}
         actual = unigram.get_counts()
         self.assertEqual(expected, actual)
 
-        unigram.add_corpus(self.s1)
+        unigram.add_corpus(self.corpus1)
         expected = {"<s>": 3, "HELLO": 2, "MY": 3, "DEAR": 2, "FRIEND": 3, "</s>": 3, "HOW": 1, "ARE": 1, "YOU": 1}
         actual = unigram.get_counts()
         self.assertEqual(expected, actual)
 
     def test_bigram(self):
         bigram = n_gram.Bigram()
-        bigram.add_corpus(self.s1)
+        bigram.add_corpus(self.corpus1)
         expected = {"<s> HELLO": 1, "HELLO MY": 1, "MY DEAR": 1, "DEAR FRIEND": 1, "FRIEND </s>": 1}
         actual = bigram.get_counts()
         self.assertEqual(expected, actual)
+
+    def test_split_corpus_lines(self):
+        ngram = n_gram.NGram()
+
+        expected = ["<s> HELLO MY DEAR FRIEND </s>"]
+        actual = list(ngram.split_corpus_sentences(self.corpus1))
+
+        self.assertEqual(expected, actual, "Split corpus should return one sentence")
+
+        expected = ["<s> HELLO MY DEAR FRIEND </s>", "<s> HOW ARE YOU MY FRIEND </s>"]
+        actual = list(ngram.split_corpus_sentences(self.corpus2))
+
+        self.assertEqual(expected, actual, "Split corpus should return two sentences")
+
+    def test_split_sentence_n_grams(self):
+        ngram = n_gram.NGram()
+
+        expected = ["<s>",  "HELLO", "MY",  "DEAR",  "FRIEND",  "</s>"]
+        actual = list(ngram.split_sentence_n_grams(self.corpus1, 1))
+
+        self.assertEqual(expected, actual, "Split sentence should return list of one words")
+
+        expected = ["<s> HELLO", "HELLO MY", "MY DEAR", "DEAR FRIEND", "FRIEND </s>"]
+        actual = list(ngram.split_sentence_n_grams(self.corpus1, 2))
+
+        self.assertEqual(expected, actual, "Split sentence should return list of two words")
