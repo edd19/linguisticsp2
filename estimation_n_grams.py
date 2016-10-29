a570adca463827d47c61126db15810f75de99391
edd19 <eddyndizera@gmail.com>
2016-10-29 16:28:18 +0200

    rearranged files

--- estimation_n_grams.py
+++ estimation_n_grams.py
@@ -56,3 +56,26 @@ class MaxLikelihood(object):
         if self.n > 1:
             self.n_minus_1_grams.flush()
         self.cache.clear()
+
+
+class LaplaceSmoothing(MaxLikelihood):
+    def __init__(self, n=1):
+        self.vocabulary = n_gram.Unigram()
+        super(self.__class__, self).__init__(n=n)
+
+    def add_training_corpus(self, corpus):
+        self.vocabulary.add_corpus(corpus)
+        super(self.__class__, self).add_training_corpus(corpus)
+
+    def compute_probability(self, ngram):
+        count_n_gram = self.get_n_gram_count(ngram)
+        count_n_minus_gram = self.get_n_minus_gram_count(ngram)
+        vocabulary_size = self.vocabulary.get_size()
+        if self.n == 1:
+            return (count_n_gram + 1) / (self.n_grams.get_word_occurences() + vocabulary_size)
+        else:
+            return (count_n_gram + 1) / (count_n_minus_gram + vocabulary_size)
+
+    def flush(self):
+        self.vocabulary.flush()
+        super(self.__class__, self).flush()
--- test/test_estimation_n_grams.py
+++ test/test_estimation_n_grams.py
@@ -2,6 +2,7 @@
 
 import unittest
 import estimation_n_grams
+import unittest
 
 
 class TestMaximumLikelihood(unittest.TestCase):
@@ -70,3 +71,40 @@ class TestMaximumLikelihood(unittest.TestCase):
         actual = max_likelihood.get_minus_n_gram(four_gram)
 
         self.assertEqual(expected, actual, "Should return a reduce a three_gram")
+
+
+class TestLaplaceSmoothing(unittest.TestCase):
+    def setUp(self):
+        self.corpus1 = "<s> HELLO MY DEAR FRIEND </s>"
+        self.corpus2 = "<s> HELLO MY DEAR FRIEND </s>\n<s> HOW ARE YOU MY FRIEND </s>"
+        self.corpus3 = "<s> THIS THIS IS MY MY VICTORY THIS NIGHT </s>"
+        self.corpus4 = "<s> HELLO MY DEAR FRIEND </s>\n<s> HOW ARE YOU MY DEAR FRIEND MY MY </s>"
+
+    def test_estimation_unigram(self):
+        laplace = estimation_n_grams.LaplaceSmoothing(1)
+
+        laplace.add_training_corpus(self.corpus1)
+
+        actual = laplace.estimate_n_gram("HELLO")
+        expected = 2/12
+        self.assertEqual(expected, actual, "Don't return good probability")
+
+        actual = laplace.estimate_n_gram("YOUNG")
+        expected = 1/12
+        self.assertEqual(expected, actual, "Don't return good probability")
+
+    def test_estimation_trigram(self):
+        laplace = estimation_n_grams.LaplaceSmoothing(3)
+
+        laplace.add_training_corpus(self.corpus2)
+
+        actual = laplace.estimate_n_gram("HELLO MY DEAR")
+        expected = 2 / 10
+        self.assertEqual(expected, actual, "Don't return good probability")
+
+        laplace.flush()
+        laplace.add_training_corpus(self.corpus4)
+
+        actual = laplace.estimate_n_gram("MY DEAR FRIEND")
+        expected = 3 / 11
+        self.assertEqual(expected, actual, "Don't return good probability")
